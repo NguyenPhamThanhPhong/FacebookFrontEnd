d7d3d68ceaad0695d9de4ddde1ab26a0d1deade3
Phong <thanhphong17160@gmail.com>
2023-11-22 12:06:40 +0700

    Messagebox demo Phong

--- src/pages/messages/messages.js
+++ src/pages/messages/messages.js
@@ -1,13 +1,56 @@
 
+import React, { useState } from 'react'
+import "react-chat-elements/dist/main.css"
+import { MessageBox } from "react-chat-elements";
 
 
 
-function messages(props) {
+function Messages(props) {
+
+    const [users, setUsers] = useState([
+        "hello1",
+        "hello2",
+        "hello3",
+        "hello4",
+        "hello5"
+    ])
+
+    function handleClick() {
+        setUsers([...users, "helloadded"])
+    }
 
     return (
         <div>
             <h1>Messages</h1>
+            <div>
+                <MessageBox
+                    position='left'
+                    title='Burhan'
+                    type='text'
+                    text="Hi there !"
+                    date={new Date()}
+                    replyButton={true}
+                />
+
+                <MessageBox
+                    position="right"
+                    title="Emre"
+                    type="meetingLink"
+                    text="Click to join the meeting"
+                    date={new Date()}
+                />
+            </div>
+            <div>
+                {
+                    users.map((item, index) => {
+                        return (
+                            <div key={index}>{item} </div>
+                        )
+                    })
+                }
+            </div>
+            <button onClick={handleClick} > check this out</button>
         </div>
     )
 }
-export default messages
+export default Messages
