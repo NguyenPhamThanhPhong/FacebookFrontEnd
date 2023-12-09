745f989604cc8735f6560a5ea161b577bbf61240
AnhViet <anhviet0307@gmail.com>
2023-12-09 19:11:45 +0700

    Initiate Avatar-bar

---
+++ src/components/avatar-bar/avatar-bar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./avatar-bar.css";
+
+function Avatar-Bar() {
+  return (
+    <>
+    <div>
+      <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+      </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Coconut</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+    </div>
+    </>
+  );
+}
+export default AvatarBar;
