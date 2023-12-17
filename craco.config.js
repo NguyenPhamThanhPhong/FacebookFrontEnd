8064acdc695528f83ed1e317e51fc83f972bc343
Phong <thanhphong17160@gmail.com>
2023-12-17 11:19:01 -0800

    done login

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,8 @@ module.exports = {
       '@pages': path.resolve(__dirname, './src/pages'),
       '@data': path.resolve(__dirname, './src/data'),
       '@root': path.resolve(__dirname, './src'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@data-store': path.resolve(__dirname, './src/data-store'),
     '@components': path.resolve(__dirname, './src/components'),
     },
   },
---
+++ src/data-store/Constants.js
@@ -0,0 +1,6 @@
+export const set_user = "SET_USER";
+export const set_posts = "SET_POSTS"; 
+export const set_conversations = "SET_CONVERSATIONS";
+export const set_friends = "SET_FRIENDS";
+export const set_people = "SET_PEOPLE";
+
