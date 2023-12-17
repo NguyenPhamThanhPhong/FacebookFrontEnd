831fcd4a2fd953d8033ee4fafd2f9a6882d02ace
Phong <thanhphong17160@gmail.com>
2023-12-17 08:10:30 -0800

    update for data backup

---
+++ src/data/login-api.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import APIUtil from './util-api';
+
+const registerUser = async (userData) => {
+  try {
+    const form = APIUtil.GenerateFormData(userData);
+
+    const response = await axios.post(APIUtil.baseURL+'/register', form, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error('Registration failed:', error);
+  }
+};
+
+const loginUser = async (username,password) => {
+    try{
+        const loginRequest={
+            username,password
+        }
+        const response = await axios.post(APIUtil.baseURL+'/login', loginRequest)
+        return response;
+    }
+    catch(error){
+        console.error('Login failed:', error);
+        throw error;
+    }
+}
+
+const sendMailVerification = async (email) => {
+    try{
+        const response = await axios.post(APIUtil.baseURL+'/send-mail-verification', email)
+        return response;
+    }
+    catch(error){
+        console.error('Login failed:', error);
+        return error;
+    }
+}
+
+const confirmMail = async (id,token) => {
+    try{
+        const response = await axios.post(APIUtil.baseURL+`/confirm-mail/${id,token}`, token)
+        return response;
+    }
+    catch(error){
+        console.error('Login failed:', error);
+        return error;
+    }
+}
+
+
+
+const LoginAPICall = {
+    register: registerUser,
+    login: loginUser,
+    sendMailVerification,
+    confirmMail
+  };
+  
+
+export default LoginAPICall;
---
+++ src/data/util-api.js
@@ -0,0 +1,36 @@
+
+
+function GenerateFormData(data){
+    const form = new FormData();
+    for (const key in data) {
+      form.append(key, data[key]);
+    }
+    return form;
+}
+const baseURL='https://localhost:7095'
+
+const jsonHeader={
+    Headers:{
+        'Content-Type': 'application/json'
+    }
+}
+const formdataHeader={
+    Headers:{
+        'Content-Type': 'multipart/form-data'
+    }
+}
+const APIUtil={
+    baseURL,
+    GenerateFormData,
+    jsonHeader,
+    formdataHeader
+}
+
+
+
+export default APIUtil
+
+export {
+    GenerateFormData,
+    jsonHeader,
+    formdataHeader}
