ac2b1f016cd7bd8f7e3419aae02e958ef987b9c5
AnhViet <anhviet0307@gmail.com>
2024-01-15 11:11:39 +0700

    Config more Groupchat

--- src/components/phong-messages-components/chat-window/Groupchat-window.js
+++ src/components/phong-messages-components/chat-window/Groupchat-window.js
@@ -2,13 +2,13 @@ import './Groupchat-window.css';
 
 import ChatTextbox from '../chat-window/Chat-textbox';
 import RoundButton from '@root/components/phong-messages-components/Round-button';
-import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBellSlash, faCircleInfo, faPhone, faVideo } from '@fortawesome/free-solid-svg-icons'
+import { faBellSlash, faCamera, faCircleInfo, faPhone, faVideo } from '@fortawesome/free-solid-svg-icons'
 import { MessageBox, Avatar } from "react-chat-elements";
 import React, { useState, useEffect, useRef } from 'react'
-
+import { Button } from 'react-bootstrap';
 import { useDataHook } from '../../../data-hook'
 import { useGlobalContext } from '../../../data-store';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
@@ -20,8 +20,18 @@ function GroupchatWindow(props) {
     let hostId = props.hostId;
     const [globalState, dispatchGlobalState] = useGlobalContext();
     const [searchInput, setSearchInput] = useState('');
-    const [selectedFriends,setSelectedFriends] = useState([]);
-        
+    const [selectedFriends, setSelectedFriends] = useState([]);
+    const fileInputRef = useRef(null);
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        // Do something with the selected file
+        console.log('Selected File:', file.name);
+    };
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -31,18 +41,34 @@ function GroupchatWindow(props) {
 
     const [isInputFocused, setIsInputFocused] = useState(false);
 
-        const handleInputFocus = () => {
-            setIsInputFocused(true);
-        };
+    const handleInputFocus = () => {
+        setIsInputFocused(true);
+    };
 
-            const handleInputBlur = () => {
-                setIsInputFocused(false);
-            };
+    const handleInputBlur = () => {
+        setIsInputFocused(false);
+    };
     // Filter friends based on search input
     const filteredFriends = people && people.filter(friend =>
         friend.personalInfo?.name && friend.personalInfo?.name.toLowerCase().includes(searchInput.toLowerCase())
     );
 
+    const {updatePersonalInfo} = useDataHook();
+    const [GroupChatName, setGroupChatName] = useState('');
+    const [blobGroupChatPicture, setblobGroupChatPicture] = useState('');
+
+    const handleClear = () => {
+        setGroupChatName('');
+        setblobGroupChatPicture(null);
+      }
+
+    const handleSaveChanges = () => {
+        console.log(GroupChatName);
+        console.log(blobGroupChatPicture);
+        handleClear();
+        handleClose();
+        updatePersonalInfo();
+      }
 
     let myMessages = [{ position: '', text: "\n" }, { position: 'right' }, { position: '' }, { position: '' }, { position: '' }, { position: '' }]
 
@@ -50,49 +76,64 @@ function GroupchatWindow(props) {
 
         <>
             <button className='group-chat-add' onClick={handleShow}>
-            <FontAwesomeIcon icon={faPenToSquare}/>
-                </button>
-
+                <FontAwesomeIcon icon={faPenToSquare} />
+            </button>
             <Modal size='lg' centered show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create a group chat </Modal.Title>
+                </Modal.Header>
                 <Modal.Body>
                     <div className='chat-window' >
-                        <div className='chat-header'>
                         <div className='friend-search'>
-      <input
-        type="text"
-        placeholder="To :"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-      />
-      {isInputFocused && filteredFriends.length > 0 && (
-        <div className='search-results'>
-          {filteredFriends.map((friend, index) => (
-            <div key={index} className='friend-result'>
-              {/* You can customize the appearance of each friend result */}
-              <Avatar
-                src={friend.avatarUrl || 'https://www.w3schools.com/howto/img_avatar.png'}
-                alt="avatar"
-                size="medium"
-                type="circle"
-              />
-              <span className=''>{friend?.personalInfo?.name}</span>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-
-                            <div className='chat-button-group'>
-                                <RoundButton width={'40px'} height={'40px'}
-                                    iconWidth={'80%'} iconHeight={'80%'} iconColor={'red'} icon={faPhone} />
-                                <RoundButton width={'40px'} height={'40px'}
-                                    iconWidth={'80%'} iconHeight={'80%'} iconColor={'red'} icon={faVideo} />
-                                <RoundButton width={'40px'} height={'40px'}
-                                    iconWidth={'80%'} iconHeight={'80%'} iconColor={'red'} icon={faCircleInfo} />
-                            </div>
+                        <div className="group-chat-add">
+                            <button className="group-chat-add" onClick={handleButtonClick}>
+                                <FontAwesomeIcon icon={faCamera} />
+                            </button>
+                            <input
+                                ref={fileInputRef}
+                                id="file-input"
+                                type="file"
+                                accept="image/*"
+                                onChange={handleFileChange}
+                                style={{ display: 'none' }}
+                            />
                         </div>
+
+                        <input
+                            type="text"
+                            placeholder="Group chat's name"
+                            value={searchInput}        
+                        />
+                        </div>
+                        
+                        <div className='friend-search'>
+                            <input
+                                type="text"
+                                placeholder="To :"
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                onFocus={handleInputFocus}
+                                onBlur={handleInputBlur}
+                            />
+                            {isInputFocused && filteredFriends.length > 0 && (
+                                <div className='search-results'>
+                                    {filteredFriends.map((friend, index) => (
+                                        <div key={index} className='friend-result'>
+                                            {/* You can customize the appearance of each friend result */}
+                                            <Avatar
+                                                src={friend.avatarUrl || 'https://www.w3schools.com/howto/img_avatar.png'}
+                                                alt="avatar"
+                                                size="medium"
+                                                type="circle"
+                                            />
+                                            <span className=''>{friend?.personalInfo?.name}</span>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                        
+
                         <div className='chat-window-chat-body'>
                             {selectedConversation?.messages && selectedConversation?.messages.map((message, index) => {
                                 let position = message.senderID === hostId ? 'right' : 'left';
@@ -121,7 +162,12 @@ function GroupchatWindow(props) {
                             })}
                         </div>
                         <div className='chat-footer'>
-                            <ChatTextbox conversation={props.conversation} />
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleSaveChanges}>
+                                Create
+                            </Button>
                         </div>
                     </div>
                 </Modal.Body>
